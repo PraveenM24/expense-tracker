a3078b567bc78be6a3ce67a843c63546dbf5dad7
Praveen M <69027747+PraveenM24@users.noreply.github.com>
2021-05-17 10:43:34 +0530

    Update Firebase.js

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,12 +1,7 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDH8RThqwDdaKWAGc_sVv-eunmcXbXK44s",
-    authDomain: "expense-tracker-20df3.firebaseapp.com",
-    projectId: "expense-tracker-20df3",
-    storageBucket: "expense-tracker-20df3.appspot.com",
-    messagingSenderId: "96007637032",
-    appId: "1:96007637032:web:31595876e9ed8e151d0d1d"
+    /*firebase credentials*/
   };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
